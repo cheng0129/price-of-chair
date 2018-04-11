162d00516c2b4c7c9cbb4a5a277ed58ec7b3c15a
youngtris <youngtris655@gmail.com>
2018-04-11 04:50:44 -0400

    something new

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,6 +1,12 @@
 
-URL = "https://api.mailgun.net/v3/sandboxd96a45a568284c1e9c7d2b376278bbe5.mailgun.org/messages"
-API_KEY = "key-cdc5b6adbc885db7184620ecfe51b74e"
-FROM = "Mailgun Sandbox <postmaster@sandboxd96a45a568284c1e9c7d2b376278bbe5.mailgun.org>"
+# URL = "https://api.mailgun.net/v3/sandboxd96a45a568284c1e9c7d2b376278bbe5.mailgun.org/messages"
+# API_KEY = "key-cdc5b6adbc885db7184620ecfe51b74e"
+# FROM = "Mailgun Sandbox <postmaster@sandboxd96a45a568284c1e9c7d2b376278bbe5.mailgun.org>"
+import os
+
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
+
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
