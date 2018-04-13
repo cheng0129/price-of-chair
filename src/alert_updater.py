6948682f053f0c54bcaff8a728b47e819db4fced
cheng0129 <cqian@emich.edu>
2018-04-13 05:21:12 -0400

    Update Files.

--- src/alert_updater.py
+++ src/alert_updater.py
@@ -3,21 +3,11 @@ from src.models.alerts.alert import Alert
 
 
 Database.initialize()
-# print("something has to be changed")
 alerts_needing_update = Alert.find_needing_update()
 
 
 for alert in alerts_needing_update:
-    # print("email sent for test!")
     alert.load_item_price()
     alert.send_email_if_price_reached()
 
-# def runASample():
-#     Database.initialize()
-#     print("something has to be changed")
-#     alerts_needing_update = Alert.find_needing_update()
-#
-#     for alert in alerts_needing_update:
-#         print("email sent for test!")
-#         alert.load_item_price()
-#         alert.send_email_if_price_reached()
+
